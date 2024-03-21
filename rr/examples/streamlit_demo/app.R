fc09289b86a85204e1c1ad0ab19ab5513680ee48
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-03-21 14:05:44 -0700

    Create app.R

---
+++ rr/examples/streamlit_demo/app.R
@@ -0,0 +1,31 @@
+
+import folium, geopy
+import streamlit as st
+from branca.element import Figure
+
+def map_loc( address ):
+  '''
+  Creates a map
+  '''
+
+  geolocator = geopy.geocoders.Nominatim(user_agent="3")
+  location = geolocator.geocode( address )         
+  lx,ly=location.longitude, location.latitude
+
+  fig = Figure( width=400,height=200)
+  a_map = folium.Map(location = [lx,ly], zoom_start = 16)
+
+  p  = geopy.point.Point(lx, ly)
+  gl = geopy.geocoders.Nominatim(user_agent="my_test") # Without the user_agent it raises a ConfigurationError.
+  site = gl.reverse(p)
+  site_name = site[0]
+  folium.Marker( location=[lx, ly], popup='Default popup Marker3',tooltip=site_name).add_to(m)
+  fig.add_child(a_map)
+  return fig, a_map, site_name
+
+
+fig, a_map, site_name = map_loc(address)
+
+st.header( 'My favourite restaurant' )
+st.markdown( 'readme.md' )
+st.st_folium( a_map )
