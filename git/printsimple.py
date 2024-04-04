2d6dd28efaa58c70f734d395f6812ca58c1bb6b0
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-04-03 21:26:05 -0700

    Create printsimple.py

---
+++ git/printsimple.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def read_csv(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        return df, None
+    except Exception as e:
+        return None, e
+
+def main():
+    print("================")
+    print("Alice & Bob's Pet Family!")
+
+    # Reading first CSV file
+    df1, error1 = read_csv('Data/cat.csv')
+    if df1 is not None:
+        print("### Cat Eating Schedule:")
+        print(df1)
+    else:
+        error(f"Error reading cat.csv: {error1}")
+
+    # Reading second CSV file
+    df2, error2 = read_csv('Data/dog.csv')
+    if df2 is not None:
+        print("### Dog Walking Schedule:")
+        print(df2)
+    else:
+        error(f"Error reading dog2.csv: {error2}")
+
+if __name__ == "__main__":
+  # Anything placed here will never be executed in a module context.
+  main()
