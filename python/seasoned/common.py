05338a1e651a76c73dfda6aa1468b7023cc88971
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-07-16 20:10:33 -0700

    Create common.py

---
+++ python/seasoned/common.py
@@ -0,0 +1,20 @@
+!/usr/bin/python
+
+## Standard
+import numpy as np
+import pandas as pd
+import polars as pol
+
+## Packages common for web scraping
+import requests, re
+
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+## Admin packages
+import os, sys, json, pickle, timeit
+from glob import glob
+from importlib import reload  
+
+
+
